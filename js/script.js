fabdaa61502f380449c52664f148ef912bc2e481
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2024-12-20 10:12:24 +0100

    ajout exercice

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// data à travailler : il s'agit d'un objet contenant un tableau de membres
+const data = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+        {
+            "name": "Molecule Man",
+            "age": 29,
+            "secretIdentity": "Dan Jukes",
+            "powers": [
+                "Radiation resistance"
+            ]
+        },
+        {
+            "name": "Madame Uppercut",
+            "age": 39,
+            "secretIdentity": "Jane Wilson",
+            "powers": [
+                "Million tonne punch"
+            ]
+        },
+        {
+            "name": "Eternal Flame",
+            "age": 1000000,
+            "secretIdentity": "Unknown",
+            "powers": [
+                "Immortality"
+            ]
+        },
+        {
+            "name": "Fatality",
+            "age": 33,
+            "secretIdentity": "Isaac Mewton",
+            "powers": [
+                "Fatality resistance"
+            ]
+        },
+        {
+            "name": "Running Lady",
+            "age": 24,
+            "secretIdentity": "Britney Sears",
+            "powers": [
+                "Speed of light"
+            ]
+        }
+    ]
+}
+
+data.squadName 
+data.homeTown 
+data.formed 
+data.secretBase 
+data.active
+
+for (var i = 0; i < data.members.length; i++) {
+    console.log(data.members[i].name)
+    console.log(data.members[i].age)
+    console.log(data.members[i].secretIdentity)
+    console.log(data.members[i].powers)
+}
+
