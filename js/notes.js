a165e26832e5d5be817b45c29b35efd4713ad67a
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2024-12-21 15:13:26 +0100

    ajout exercice

--- js/notes.js
+++ js/notes.js
@@ -1,3 +1,16 @@
 https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
 https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
 https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON
+
+// data.squadName 
+// data.homeTown 
+// data.formed 
+// data.secretBase 
+// data.active
+
+// for (var i = 0; i < data.members.length; i++) {
+//     console.log(data.members[i].name)
+//     console.log(data.members[i].age)
+//     console.log(data.members[i].secretIdentity)
+//     console.log(data.members[i].powers)
+// }
--- js/script.js
+++ js/script.js
@@ -49,48 +49,74 @@ const data = {
     ]
 }
 
-// data.squadName 
-// data.homeTown 
-// data.formed 
-// data.secretBase 
-// data.active
+
+
+// function showdata(data) {
+// const members = data["members"];
 
 // for (var i = 0; i < data.members.length; i++) {
-//     console.log(data.members[i].name)
-//     console.log(data.members[i].age)
-//     console.log(data.members[i].secretIdentity)
-//     console.log(data.members[i].powers)
+//     document.getElementById("mycard").innerHTML += `
+//             <div class="card-body">
+//                 <h5 class="card-title">${myh5}</h5>
+//                 <p class="card-text">
+//                     <ul>
+//                         <li><b>Secret identity :</b> ${myl2}</li>
+//                         <li><b>Age :</b> ${myl1}</li>
+//                         <li><b>Superpowers :</b></b>
+//                         <ul>
+//                             <li>${myl3}</li>
+//                         </ul>
+//                         </li>
+//                     </ul>
+//                 </p>
+//             </div>
+// `
 // }
 
-function showdata(jsonObj) {
-    var members = jsonObj["members"];
-  
-    for (var i = 0; i < data.members.length; i++) {
-      var mycard = document.createElement("card");
-      var myh5 = document.createElement("h5");
-      var mylist = document.createElement("ul");
-      var myl1 = document.createElement("li");
-      var myl2 = document.createElement("li");
-      var myl3 = document.createElement("li");
-  
-      myh5.textContent = members[i].name;
-      myl1.textContent = "Age: " + members[i].age;
-      myl2.textContent = "Secret identity: " + members[i].secretIdentity;
-      myl3.textContent = "Superpowers:";
-  
-      var superPowers = members[i].powers;
-      for (var j = 0; j < superPowers.length; j++) {
-        var listItem = document.createElement("li");
-        listItem.textContent = superPowers[j];
-        myList.appendChild(listItem);
-      }
+//     for (var i = 0; i < data.members.length; i++) {
+//         var mycard = document.createElement("card");
+//         var myh5 = document.createElement("h5");
+//         var mylist = document.createElement("ul");
+//         var myl1 = document.createElement("li");
+//         var myl2 = document.createElement("li");
+//         var myl3 = document.createElement("li");
 
-      mycard.appendChild(myH5);
-      mycard.appendChild(myl1);
-      mycard.appendChild(myl2);
-      mycard.appendChild(myl3);
-      mycard.appendChild(mylist);
-  
-      section.appendChild(mycard);
-    }
+//         myh5.textContent = members[i].name;
+//         myl1.textContent = "Age: " + members[i].age;
+//         myl2.textContent = "Secret identity: " + members[i].secretIdentity;
+//         myl3.textContent = "Superpowers:";
+
+//         var superPowers = members[i].powers;
+//         for (var j = 0; j < superPowers.length; j++) {
+//             var listItem = document.createElement("li");
+//             listItem.textContent = superPowers[j];
+//             myList.appendChild(listItem);
+//         }
+//     }
+// }
+
+
+function showdata(data) {
+const members = data["members"]
+
+for (var i = 0; i < data.members.length; i++) {
+    document.getElementById("mycard").innerHTML += `
+            <div class="card-body">
+                <h5 class="card-title">${members.name}</h5>
+                <p class="card-text">
+                    <ul>
+                        <li><b>Secret identity :</b> ${members.secretIdentity}</li>
+                        <li><b>Age :</b> ${members.age}</li>
+                        <li><b>Superpowers :</b></b>
+                        <ul>
+                            <li>${members.powers}</li>
+                        </ul>
+                        </li>
+                    </ul>
+                </p>
+            </div>
+`
 }
+}
+
+showdata(data);
